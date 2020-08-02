cb7cb6fade512c5df30d194ded6e7ed4bf95b84d
ophiuchus2 <ophiuchus@gmx.com>
2020-08-02 09:46:00 -0500

    change license to AGPLv3 (or later)

--- SynchronizedChannel.h
+++ SynchronizedChannel.h
@@ -1,11 +1,20 @@
-/**
-* @file SynchronizedChannel.h
-* @author Ophiuchus2
-* @date 28 Jun 2020
-* @copyright 2020 Wrongthink
-* @brief <brief>
-*/
+/*
+This file is part of Wrongthink.
 
+Wrongthink - Modern, open & performant chat protocol. Based on gRPC.
+Copyright (C) 2020 Ophiuchus2
+
+This program is free software: you can redistribute it and/or modify it under the
+terms of the GNU Affero General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <mutex>
 #include <string>
 #include <vector>
