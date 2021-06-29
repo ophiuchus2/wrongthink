bd55505b9b8ccd4949f855eb0c5277edcf36e6e7
ophiuchus2 <ophiuchus@gmx.com>
2021-06-29 08:59:54 -0500

    move athenticator to own cpp file

---
+++ Authentication/WrongthinkTokenAuthenticator.cpp
@@ -0,0 +1,60 @@
+#include "WrongthinkTokenAuthenticator.h"
+
+namespace WrongthinkTokenAuth {
+
+const std::string AUTH_UNAME_KEY = "auth-uname";
+const std::string AUTH_TOKEN_KEY = "auth-token";
+
+grpc::Status WrongthinkClientTokenPlugin::GetMetadata(
+    grpc::string_ref service_url, grpc::string_ref method_name,
+    const grpc::AuthContext& channel_auth_context,
+    std::multimap<grpc::string, grpc::string>* metadata) {
+  metadata->insert(std::make_pair(AUTH_UNAME_KEY, uname_));
+  metadata->insert(std::make_pair(AUTH_TOKEN_KEY, token_));
+  return grpc::Status::OK;
+}
+
+grpc::string WrongthinkClientTokenPlugin::DebugString() {
+  return "uname: " + uname_ + ", token: " + token_;
+}
+
+// Interface implementation
+bool WrongthinkAuthMetadataProcessor::IsBlocking() const { return is_blocking_; }
+
+Status WrongthinkAuthMetadataProcessor::Process(const grpc::AuthMetadataProcessor::InputMetadata& auth_metadata, grpc::AuthContext* context,
+                grpc::AuthMetadataProcessor::OutputMetadata* consumed_auth_metadata,
+                grpc::AuthMetadataProcessor::OutputMetadata* response_metadata) {
+  for (auto& p : auth_metadata) {
+    std::string key(p.first.data(), p.first.length());
+    std::string value(p.second.data(), p.second.length());
+    context->AddProperty(key, value);
+    consumed_auth_metadata->insert(std::make_pair(key, value));
+  }
+  return Status::OK;
+}
+
+bool hasCredentials(ServerContext* context) {
+  auto cmeta = context->client_metadata();
+  if (cmeta.count(AUTH_UNAME_KEY) == 0)
+    return false;
+  if (cmeta.count(AUTH_TOKEN_KEY) == 0)
+    return false;
+  return true;
+}
+
+std::pair<std::string, std::string> getCredentials(ServerContext* context) {
+  auto cmeta = context->client_metadata();
+
+  auto uname = cmeta.find(AUTH_UNAME_KEY);
+  std::string s1(uname->second.data(), uname->second.length());
+  auto token = cmeta.find(AUTH_TOKEN_KEY);
+  std::string s2(token->second.data(), token->second.length());
+  return std::make_pair(s1,s2);
+}
+
+void addCredentials(grpc::ClientContext* context, WrongthinkUser* user) {
+  context->AddMetadata(AUTH_UNAME_KEY, user->uname());
+  context->AddMetadata(AUTH_TOKEN_KEY, user->token());
+}
+
+} // namespace WrongthinkTokenAuth
--- Authentication/WrongthinkTokenAuthenticator.h
+++ Authentication/WrongthinkTokenAuthenticator.h
@@ -3,11 +3,21 @@
 
 #include <grpcpp/security/credentials.h>
 #include <grpcpp/security/auth_metadata_processor.h>
+#include <grpcpp/grpcpp.h>
+#include "spdlog/spdlog.h"
+#include "wrongthink.grpc.pb.h"
+
+// grpc using statements
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerWriter;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::Status;
+using grpc::StatusCode;
 
 namespace WrongthinkTokenAuth {
-
-const std::string AUTH_UNAME_KEY = "auth-uname";
-const std::string AUTH_TOKEN_KEY = "auth-token";
 
 class WrongthinkClientTokenPlugin : public grpc::MetadataCredentialsPlugin {
  public:
@@ -17,15 +27,9 @@ class WrongthinkClientTokenPlugin : public grpc::MetadataCredentialsPlugin {
   grpc::Status GetMetadata(
       grpc::string_ref service_url, grpc::string_ref method_name,
       const grpc::AuthContext& channel_auth_context,
-      std::multimap<grpc::string, grpc::string>* metadata) override {
-    metadata->insert(std::make_pair(AUTH_UNAME_KEY, uname_));
-    metadata->insert(std::make_pair(AUTH_TOKEN_KEY, token_));
-    return grpc::Status::OK;
-  }
+      std::multimap<grpc::string, grpc::string>* metadata) override;
 
-  grpc::string DebugString() override {
-    return "uname: " + uname_ + ", token: " + token_;
-  }
+  grpc::string DebugString() override ;
 
  private:
   grpc::string uname_;
@@ -38,72 +42,20 @@ class WrongthinkAuthMetadataProcessor : public grpc::AuthMetadataProcessor {
   WrongthinkAuthMetadataProcessor (bool is_blocking) : is_blocking_(is_blocking) {}
 
   // Interface implementation
-  bool IsBlocking() const override { return is_blocking_; }
+  bool IsBlocking() const override;
 
   Status Process(const grpc::AuthMetadataProcessor::InputMetadata& auth_metadata, grpc::AuthContext* context,
                  grpc::AuthMetadataProcessor::OutputMetadata* consumed_auth_metadata,
-                 grpc::AuthMetadataProcessor::OutputMetadata* response_metadata) override {
-    for (auto& p : auth_metadata) {
-      std::string key(p.first.data(), p.first.length());
-      std::string value(p.second.data(), p.second.length());
-      context->AddProperty(key, value);
-      consumed_auth_metadata->insert(std::make_pair(key, value));
-    }
-    return Status::OK;
-  }
+                 grpc::AuthMetadataProcessor::OutputMetadata* response_metadata) override;
 
  private:
   bool is_blocking_;
 };
 
-static bool hasCredentials(ServerContext* context) {
-  auto cmeta = context->client_metadata();
-  if (cmeta.count(AUTH_UNAME_KEY) == 0)
-    return false;
-  if (cmeta.count(AUTH_TOKEN_KEY) == 0)
-    return false;
-  return true;
-}
-
-static std::pair<std::string, std::string> getCredentials(ServerContext* context) {
-  auto cmeta = context->client_metadata();
-
-  auto uname = cmeta.find(AUTH_UNAME_KEY);
-  std::string s1(uname->second.data(), uname->second.length());
-  auto token = cmeta.find(AUTH_TOKEN_KEY);
-  std::string s2(token->second.data(), token->second.length());
-  return std::make_pair(s1,s2);
-}
-
-static void addCredentials(grpc::ClientContext* context, WrongthinkUser* user) {
-  context->AddMetadata(AUTH_UNAME_KEY, user->uname());
-  context->AddMetadata(AUTH_TOKEN_KEY, user->token());
-}
+bool hasCredentials(ServerContext* context);
+std::pair<std::string, std::string> getCredentials(ServerContext* context);
+void addCredentials(grpc::ClientContext* context, WrongthinkUser* user);
 
 }
-
-/*
-class WrongthinkAuthService {
-public:
-
-  using AuthFunc = std::
-
-  WrongthinkAuthService(std::shared_ptr<DBInterface> db) : db_{db} {
-
-  }
-
-  bool Authenticate(const std::string& rpc, ServerContext* context) {
-
-  }
-
-  static bool isAdmin_(ServerContext* context) {
-
-  }
-private:
-
-
-
-  std::shared_ptr<DBInterface> db_;
-}*/
 
 #endif
